04245c569c4ff4b91481d5046bc1c0afddae9abd
Rafael Severiano <rafaelfsadatascientist@gmail.com>
2026-02-08 16:19:12 -0300

    update

---
+++ Easy/Final Account Balance.sql
@@ -0,0 +1,13 @@
+--DataLemur: Final Account Balance
+-- Dialect: Postgree SQL
+--Level: Easy
+--Tables: transactions(transaction_id,account_id,amount,transaction_type)
+--Concepts: Sum, Case When, Group by
+
+SELECT 
+account_id,
+sum(case when transaction_type='Deposit' then amount
+when transaction_type='Withdrawal' then -amount
+end) as final_balance
+FROM transactions
+group by 1
