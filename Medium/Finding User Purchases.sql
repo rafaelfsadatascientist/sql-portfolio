2d521e32c3e6f9614a196d52b044781cb7b675d1
Rafael Severiano <rafaelfsadatascientist@gmail.com>
2026-02-09 14:33:58 -0300

    update

---
+++ Medium/Finding User Purchases.sql
@@ -0,0 +1,18 @@
+--StrataScratch: Finding User Purchases
+-- Dialect: PostgreSQL
+--Level: Medium
+--Tables: amazon_transactions(created_at,id,item,revenue,user_id)
+--Concepts: CTEs, Ranks, Window Functions, Between 
+
+WITH auxiliar_cte AS (
+    SELECT
+        user_id,
+        created_at - MIN(created_at) OVER (PARTITION BY user_id) AS intervals,
+        DENSE_RANK() OVER (PARTITION BY user_id ORDER BY created_at ASC) AS created_at_rank
+    FROM amazon_transactions
+)
+SELECT
+    user_id
+FROM auxiliar_cte
+WHERE created_at_rank = 2
+  AND intervals BETWEEN 1 AND 7
---
+++ Medium/No Order Customers.sql
@@ -0,0 +1,20 @@
+--StrataScratch: No Order Customers
+-- Dialect: PostgreSQL
+--Level: Medium
+--Tables: customer(address,city,first_name,id,last_name,phone_number) & orders(cust_id,id,order_date,order_details,total_order_cost)
+--Concepts: CTEs, Joins, Where, NULLS
+
+WITH customers_in_date_range AS (
+    SELECT 
+        customers.id 
+    FROM customers 
+    INNER JOIN orders 
+        ON customers.id = orders.cust_id 
+    WHERE order_date BETWEEN '2019-02-01' AND '2019-03-01'
+)
+SELECT 
+    customers.first_name
+FROM customers
+LEFT JOIN customers_in_date_range
+    ON customers.id = customers_in_date_range.id
+WHERE customers_in_date_range.id IS NULL
