60561e223124e888e4d5f97e047729b9f5666c8e
Rafael Severiano <rafaelfsadatascientist@gmail.com>
2026-02-09 10:14:47 -0300

    update

---
+++ Hard/Best Selling Item.sql
@@ -0,0 +1,33 @@
+--StrataScratch: Best Selling Item
+-- Dialect: PostgreSQL
+--Level: Hard
+--Tables: online_retail(country,customerid,description,invoicedate,invoiceno,quantity,stockcode,unitprice)
+--Concepts: CTEs, Like, Rank
+
+WITH total_sales_amount AS (
+    SELECT
+        EXTRACT(MONTH FROM invoicedate) AS month,
+        description,
+        SUM(unitprice * quantity) AS total_sales_amount
+    FROM online_retail
+    WHERE invoiceno NOT LIKE 'C%'
+    GROUP BY
+        1,2
+),
+creating_rank AS (
+    SELECT
+        month,
+        description,
+        total_sales_amount,
+        DENSE_RANK() OVER (
+            PARTITION BY month
+            ORDER BY total_sales_amount DESC
+        ) AS total_sales_amount_rank
+    FROM total_sales_amount
+)
+SELECT
+    month,
+    description,
+    total_sales_amount
+FROM creating_rank
+WHERE total_sales_amount_rank = 1
