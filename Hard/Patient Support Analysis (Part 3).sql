42dfb70d59b804124ba32570bdbedcad395aa31a
Rafael Severiano <rafaelfsadatascientist@gmail.com>
2026-02-09 16:41:51 -0300

    update

---
+++ Hard/Patient Support Analysis (Part 3).sql
@@ -0,0 +1,20 @@
+--DataLemur: Patient Support Analysis (Part 3)
+-- Dialect: PostgreSQL
+--Level: Hard
+--Tables: callers(policy_holder_id,case_id,call_category,call_date,call_duration_secs)
+--Concepts: LAG, DISTINCT, INTERVALs
+
+WITH previous_call_date_CTE AS (
+    SELECT
+        policy_holder_id,
+        call_date,
+        LAG(call_date) OVER (
+            PARTITION BY policy_holder_id
+            ORDER BY call_date
+        ) AS previous_call_date
+    FROM callers
+)
+SELECT
+    COUNT(DISTINCT policy_holder_id) AS policy_holder_count
+FROM previous_call_date_CTE
+WHERE call_date - previous_call_date <= INTERVAL '7 days'
---
+++ Hard/Patient Support Analysis (Part 4).sql
@@ -0,0 +1,47 @@
+--DataLemur: Patient Support Analysis (Part 4)
+-- Dialect: PostgreSQL
+--Level: Hard
+--Tables: callers(policy_holder_id,case_id,call_category,call_date,call_duration_secs)
+--Concepts: LAG, ROUND, CTEs
+
+WITH long_calls_CTE AS (
+    SELECT
+        case_id,
+        EXTRACT(YEAR FROM call_date)  AS yr,
+        EXTRACT(MONTH FROM call_date) AS mth
+    FROM callers
+    WHERE call_duration_secs > 300
+),
+
+count_CTE AS (
+    SELECT
+        yr,
+        mth,
+        COUNT(case_id) AS case_id_count
+    FROM long_calls_CTE
+    GROUP BY
+        yr,
+        mth
+),
+
+LAG_CTE AS (
+    SELECT
+        *,
+        LAG(case_id_count) OVER (
+            ORDER BY yr, mth
+        ) AS previous_case_id_count
+    FROM count_CTE
+)
+
+SELECT
+    yr,
+    mth,
+    ROUND(
+        100 * (
+            (case_id_count::numeric
+            / NULLIF(previous_case_id_count, 0))
+            - 1
+        ),
+        1
+    ) AS long_calls_growth_pct
+FROM LAG_CTE
